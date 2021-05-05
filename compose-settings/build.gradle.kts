1c7e4498026e0e976dbf3985ac36ad5dc8aa0ca4
Bernat Borrás <bernat.borras@adevinta.com>
2021-05-05 21:44:38 +0200

    remove kotlin from module

--- compose-settings/build.gradle.kts
+++ compose-settings/build.gradle.kts
@@ -51,8 +51,6 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.0")
-
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.3.0")
