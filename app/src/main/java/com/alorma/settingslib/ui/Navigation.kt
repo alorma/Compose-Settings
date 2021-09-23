da41163625fa18d4139c83edf5c6302440b022df
Bernat Borrás <bernat.borras@adevinta.com>
2021-09-23 12:40:03 +0200

    Cleanup code

--- app/src/main/java/com/alorma/settingslib/ui/Navigation.kt
+++ app/src/main/java/com/alorma/settingslib/ui/Navigation.kt
@@ -1,7 +1,7 @@
 package com.alorma.settingslib.ui
 
 object Navigation {
-  val NAV_TOP_SETTINGS = "top_settings" to "top_settings"
+  val NAV_TOP_SETTINGS = "top_settings" to "Settings content"
   val NAV_MENU_LINKS = "menu_links" to "Menu links"
   val NAV_SWITCHES = "switches" to "Switches"
   val NAV_CHECKBOXES = "checkboxes" to "Checkboxes"
