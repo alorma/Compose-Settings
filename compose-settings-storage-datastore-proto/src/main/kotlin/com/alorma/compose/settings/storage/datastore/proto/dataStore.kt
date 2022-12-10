446a1c282f77a5b40797fd8f083be77ff4021c55
S-H-Y-A <74596628+S-H-Y-A@users.noreply.github.com>
2022-12-10 17:54:36 +0900

    Fix an error that "There are multiple DataStores active for the same file" error is thrown when "rememberProtoDataStoreState()" is called again.

--- compose-settings-storage-datastore-proto/src/main/kotlin/com/alorma/compose/settings/storage/datastore/proto/dataStore.kt
+++ compose-settings-storage-datastore-proto/src/main/kotlin/com/alorma/compose/settings/storage/datastore/proto/dataStore.kt
@@ -2,31 +2,44 @@ package com.alorma.compose.settings.storage.datastore.proto
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.datastore.core.DataStore
 import androidx.datastore.core.Serializer
 import androidx.datastore.dataStore
+import com.alorma.compose.settings.storage.datastore.proto.ActiveDataStore.addActiveDataStore
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 
 @Composable
-fun <T> rememberProtoDataStoreState(
+inline fun <reified T : Any> rememberProtoDataStoreState(
     context: Context = LocalContext.current,
     filename: String = "compose_settings_datastore_proto.pb",
     serializer: Serializer<T>
 ): ProtoDataStoreState<T> {
-    return remember(context, filename, serializer) {
-        ProtoDataStoreState(context, filename, serializer)
+
+    @Suppress("UNCHECKED_CAST")
+    val dataStore: DataStore<T> = remember(context, filename, serializer) {
+        (((ActiveDataStore.getActiveDataStores()[T::class] as? DataStore<T>)
+            ?: ProtoDataStoreCreateSupporter(context, filename, serializer).dataStore)).also {
+            addActiveDataStore(it, T::class)
+        }
     }
+    return rememberProtoDataStoreState(dataStore, serializer)
 }
 
 @Composable
-fun <T> rememberProtoDataStoreState(
+inline fun <reified T : Any> rememberProtoDataStoreState(
     dataStore: DataStore<T>,
     serializer: Serializer<T>
 ): ProtoDataStoreState<T> {
+    LaunchedEffect(dataStore) {
+        if (ActiveDataStore.getActiveDataStores()[T::class] == null) {
+            addActiveDataStore(dataStore, T::class)
+        }
+    }
     return remember(dataStore, serializer) {
         ProtoDataStoreState(dataStore, serializer)
     }
@@ -37,25 +50,16 @@ class ProtoDataStoreState<T>(
     val serializer: Serializer<T>
 ): ReadOnlyProperty<T?, DataStore<T>> {
 
-    constructor(
-        context: Context,
-        filename: String,
-        serializer: Serializer<T>
-    ) : this(ProtoDataStoreStateSupporter(context, filename, serializer).dataStore, serializer)
-
-    override fun getValue(thisRef: T?, property: KProperty<*>): DataStore<T> {
-        return dataStore
-    }
-
-    class ProtoDataStoreStateSupporter<T>(
-        context: Context,
-        filename: String,
-        serializer: Serializer<T>
-    ) {
-        private val Context.dataStore: DataStore<T> by dataStore(
-            fileName = filename,
-            serializer = serializer
-        )
-        val dataStore: DataStore<T> = context.applicationContext.dataStore
-    }
+    override fun getValue(thisRef: T?, property: KProperty<*>) = dataStore
+}
+class ProtoDataStoreCreateSupporter<T>(
+    context: Context,
+    filename: String,
+    serializer: Serializer<T>
+) {
+    private val Context.dataStore: DataStore<T> by dataStore(
+        fileName = filename,
+        serializer = serializer
+    )
+    val dataStore: DataStore<T> = context.applicationContext.dataStore
 }
