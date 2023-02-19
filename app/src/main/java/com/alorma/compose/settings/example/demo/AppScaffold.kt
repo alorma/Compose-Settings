cad7b46ee03fcc5027b64fa8ebcf617e2e72594a
Bernat Borrás <bernat.borras@adevinta.com>
2023-02-19 12:26:37 +0100

    Move switch to toolbar

--- app/src/main/java/com/alorma/compose/settings/example/demo/AppScaffold.kt
+++ app/src/main/java/com/alorma/compose/settings/example/demo/AppScaffold.kt
@@ -6,25 +6,19 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.consumedWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.navigation.NavController
 import com.alorma.compose.settings.example.ui.Navigation
 import com.alorma.compose.settings.storage.base.SettingValueState
 import com.alorma.compose.settings.storage.base.rememberBooleanSettingState
-import com.alorma.compose.settings.ui.SettingsSwitch
-import kotlinx.coroutines.launch
 
 @OptIn(
   ExperimentalMaterial3Api::class,
@@ -40,8 +34,6 @@ fun AppScaffold(
   snackbarHostState: SnackbarHostState = remember { SnackbarHostState() },
   content: @Composable (ColumnScope.() -> Unit),
 ) {
-  val coroutineScope = rememberCoroutineScope()
-
   val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
   Scaffold(
     modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
@@ -49,6 +41,8 @@ fun AppScaffold(
       if (title != null) {
         SettingsToolbar(
           title = title,
+          enabledState = enabledState.value,
+          onEnabledChange = { enabledState.value = it },
           showSettings = showSettings,
           onBack = onBack,
           onNavigateSettings = { navController.navigate(route = Navigation.NAV_SETTINGS.first) },
@@ -60,26 +54,12 @@ fun AppScaffold(
       SnackbarHost(hostState = snackbarHostState)
     }
   ) { innerPadding ->
-    LaunchedEffect(key1 = enabledState) {
-      coroutineScope.launch {
-        val message = "Enabled: ${enabledState.value}"
-        snackbarHostState.showSnackbar(message)
-      }
-    }
-
     Column(
       modifier = Modifier
         .consumedWindowInsets(innerPadding)
         .fillMaxSize()
         .padding(top = innerPadding.calculateTopPadding()),
-      content = {
-        SettingsSwitch(
-          state = enabledState,
-          title = { Text(text = "Enabled") },
-        )
-        Divider()
-        content()
-      },
+      content = content,
     )
   }
 }
