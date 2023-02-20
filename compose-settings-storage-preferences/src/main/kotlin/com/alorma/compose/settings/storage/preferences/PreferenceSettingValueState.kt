b8a96e0985f98ad9bd50c0b64102899af4468da7
itznotabug <itznotabug@gmail.com>
2023-02-20 21:13:31 +0530

    Create a generic `rememberComposeSettingsPreference` to persist data on local storage using SharedPreferences.

---
+++ compose-settings-storage-preferences/src/main/kotlin/com/alorma/compose/settings/storage/preferences/PreferenceSettingValueState.kt
@@ -0,0 +1,63 @@
+package com.alorma.compose.settings.storage.preferences
+
+import android.content.SharedPreferences
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
+import com.alorma.compose.settings.storage.base.SettingValueState
+
+@Composable
+fun <T> rememberComposeSettingsPreference(
+  key: String,
+  defaultValue: T,
+  delimiter: String = "|",
+  preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(LocalContext.current),
+): PreferenceSettingValueState<T> {
+  return remember {
+      PreferenceSettingValueState(
+          key = key, defaultValue = defaultValue,
+          delimiter = delimiter, preferences = preferences
+      )
+  }
+}
+
+@Suppress("unchecked_cast")
+class PreferenceSettingValueState<T>(
+  val key: String,
+  val defaultValue: T,
+  val delimiter: String = "|",
+  private val preferences: SharedPreferences,
+) : SettingValueState<T> {
+
+  private var _value by mutableStateOf(
+    if (defaultValue is Set<*>) {
+        preferences.getString(key, (defaultValue as Set<*>).toPrefString(delimiter))
+          .orEmpty().split(delimiter)
+          .filter { it.isNotEmpty() }.map { it.toInt() }
+          .toMutableSet()
+    } else preferences.all[key] as T ?: defaultValue
+  )
+
+  override var value: T
+      set(value) {
+          _value = value
+          when (value) {
+              is Boolean -> preferences.edit { putBoolean(key, value) }
+              is String -> preferences.edit { putString(key, value) }
+              is Int -> preferences.edit { putInt(key, value) }
+              is Float -> preferences.edit { putFloat(key, value) }
+              is Set<*> -> preferences.edit { putString(key, value.toPrefString(delimiter)) }
+              else -> throw IllegalArgumentException("Type ${value!!::class.simpleName} is not supported")
+          }
+      }
+      get() = _value as T
+
+  private fun Set<*>.toPrefString(delimiter: String): String {
+    return joinToString(separator = delimiter) { it.toString() }
+  }
+
+  override fun reset() {
+      value = defaultValue
+  }
+}
