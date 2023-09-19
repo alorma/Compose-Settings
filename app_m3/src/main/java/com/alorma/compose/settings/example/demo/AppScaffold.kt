3c6cc399043e2f27fbb823a40ee8442f1f04adbf
Bernat Borrás <bernat.borras@adevinta.com>
2023-09-19 08:35:39 +0200

    Deprecations

--- app_m3/src/main/java/com/alorma/compose/settings/example/demo/AppScaffold.kt
+++ app_m3/src/main/java/com/alorma/compose/settings/example/demo/AppScaffold.kt
@@ -3,7 +3,7 @@ package com.alorma.compose.settings.example.demo
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.consumedWindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,45 +21,45 @@ import com.alorma.compose.settings.storage.base.SettingValueState
 import com.alorma.compose.settings.storage.base.rememberBooleanSettingState
 
 @OptIn(
-    ExperimentalMaterial3Api::class,
-    ExperimentalLayoutApi::class,
+  ExperimentalMaterial3Api::class,
+  ExperimentalLayoutApi::class,
 )
 @Composable
 fun AppScaffold(
-    navController: NavController,
-    enabledState: SettingValueState<Boolean> = rememberBooleanSettingState(true),
-    showSettings: Boolean = true,
-    onBack: (() -> Unit)? = { navController.popBackStack() },
-    title: @Composable (() -> Unit)? = null,
-    snackbarHostState: SnackbarHostState = remember { SnackbarHostState() },
-    content: @Composable (ColumnScope.() -> Unit),
+  navController: NavController,
+  enabledState: SettingValueState<Boolean> = rememberBooleanSettingState(true),
+  showSettings: Boolean = true,
+  onBack: (() -> Unit)? = { navController.popBackStack() },
+  title: @Composable (() -> Unit)? = null,
+  snackbarHostState: SnackbarHostState = remember { SnackbarHostState() },
+  content: @Composable (ColumnScope.() -> Unit),
 ) {
-    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
-    Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            if (title != null) {
-                SettingsToolbar(
-                    title = title,
-                    enabledState = enabledState.value,
-                    onEnabledChange = { enabledState.value = it },
-                    showSettings = showSettings,
-                    onBack = onBack,
-                    onNavigateSettings = { navController.navigate(route = Navigation.NAV_SETTINGS.first) },
-                    scrollBehavior = scrollBehavior,
-                )
-            }
-        },
-        snackbarHost = {
-            SnackbarHost(hostState = snackbarHostState)
-        },
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .consumedWindowInsets(innerPadding)
-                .fillMaxSize()
-                .padding(top = innerPadding.calculateTopPadding()),
-            content = content,
+  val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+  Scaffold(
+    modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+    topBar = {
+      if (title != null) {
+        SettingsToolbar(
+          title = title,
+          enabledState = enabledState.value,
+          onEnabledChange = { enabledState.value = it },
+          showSettings = showSettings,
+          onBack = onBack,
+          onNavigateSettings = { navController.navigate(route = Navigation.NAV_SETTINGS.first) },
+          scrollBehavior = scrollBehavior,
         )
-    }
+      }
+    },
+    snackbarHost = {
+      SnackbarHost(hostState = snackbarHostState)
+    },
+  ) { innerPadding ->
+    Column(
+      modifier = Modifier
+          .consumeWindowInsets(innerPadding)
+          .fillMaxSize()
+          .padding(top = innerPadding.calculateTopPadding()),
+      content = content,
+    )
+  }
 }
