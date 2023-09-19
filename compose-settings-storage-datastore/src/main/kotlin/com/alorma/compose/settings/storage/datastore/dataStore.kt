0cd71aae77b7d04d7b37e98e9a621f8a6c1aef56
Bernat Borrás <bernat.borras@adevinta.com>
2023-09-19 08:25:21 +0200

    Remove datastore

--- compose-settings-storage-datastore/src/main/kotlin/com/alorma/compose/settings/storage/datastore/dataStore.kt
+++
@@ -1,8 +0,0 @@
-package com.alorma.compose.settings.storage.datastore
-
-import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
-
-val Context.composeSettingsDataStore: DataStore<Preferences> by preferencesDataStore(name = "compose_settings_datastore")
