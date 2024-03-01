09b7cdcb0920f05d01b421bf9dea3e3673189dc4
Bernat Borrás <bernat.borras@adevinta.com>
2024-03-01 07:41:12 +0100

    Add sample for int state

--- composeApp/src/commonMain/kotlin/com.alorma.compose.settings.sample.shared/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/com.alorma.compose.settings.sample.shared/SettingsScreen.kt
@@ -4,10 +4,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.alorma.compose.settings.storage.base.getValue
+import com.alorma.compose.settings.storage.base.setValue
+import com.alorma.compose.settings.storage.disk.rememberIntSettingState
+import com.alorma.compose.settings.ui.SettingsMenuLink
 import com.alorma.compose.settings.ui.SettingsSwitch
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -22,11 +27,23 @@ fun SettingsScreen(
         .consumeWindowInsets(padding)
         .padding(top = padding.calculateTopPadding()),
     ) {
-
       SettingsSwitch(
         state = darkTheme,
         title = { Text(text = "Dark mode") },
         onCheckedChange = onDarkThemeChange,
+      )
+      HorizontalDivider()
+
+      var intState by rememberIntSettingState(key = "clicked", defaultValue = 0)
+      SettingsMenuLink(
+        title = {
+          if (intState == 0) {
+            Text(text = "Click me")
+          } else {
+            Text(text = "Clicked $intState times")
+          }
+        },
+        onClick = { intState += 1 }
       )
     }
   }
