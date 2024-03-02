aea4828ebb05228b535137812bb6e5ab8d279bdd
Bernat Borrás <bernat.borras@adevinta.com>
2024-03-02 13:14:29 +0100

    No custom height on settings group title

--- compose-settings-ui/src/commonMain/kotlin/com/alorma/compose/settings/ui/SettingsGroup.kt
+++ compose-settings-ui/src/commonMain/kotlin/com/alorma/compose/settings/ui/SettingsGroup.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
@@ -33,7 +31,6 @@ fun SettingsGroup(
       if (title != null) {
         SettingsGroupTitle(title)
       }
-      Spacer(modifier = Modifier.height(8.dp))
       content()
     }
   }
@@ -44,8 +41,7 @@ internal fun SettingsGroupTitle(title: @Composable () -> Unit) {
   Box(
     modifier = Modifier
       .fillMaxWidth()
-      .height(64.dp)
-      .padding(horizontal = 16.dp),
+      .padding(vertical = 8.dp, horizontal = 16.dp),
     contentAlignment = Alignment.CenterStart,
   ) {
     val primary = MaterialTheme.colorScheme.primary
