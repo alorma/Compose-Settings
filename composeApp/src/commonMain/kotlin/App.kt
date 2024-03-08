7e6dff5a103bf3a206c795769d247d88a1138415
Bernat Borrás <bernat.borras@adevinta.com>
2024-03-08 08:46:48 +0100

    Update with scaffold

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,11 +1,65 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
-import theme.ComposeSettingsTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.alorma.compose.settings.storage.disk.rememberBooleanSettingState
 import com.russhwolf.settings.Settings
+import theme.ComposeSettingsTheme
 
+@OptIn(
+  ExperimentalMaterial3WindowSizeClassApi::class,
+  ExperimentalMaterial3Api::class,
+)
 @Composable
 fun App() {
+  val windowSizeClass = calculateWindowSizeClass()
+
   val settings = Settings()
-  ComposeSettingsTheme {
-    SettingsScreen(settings = settings)
+
+  val darkMode = rememberBooleanSettingState(
+    key = "darkMode",
+    defaultValue = false,
+    settings = settings,
+  )
+
+  ComposeSettingsTheme(isSystemDark = darkMode.value) {
+    Scaffold(
+      modifier = Modifier.fillMaxSize(),
+      topBar = {
+        TopAppBar(
+          title = { Text(text = "Compose settings - Sample") },
+          actions = {
+            Switch(checked = darkMode.value, onCheckedChange = { newState ->
+              darkMode.value = newState
+            })
+          },
+        )
+      },
+    ) {
+      when (windowSizeClass.widthSizeClass) {
+        WindowWidthSizeClass.Compact, WindowWidthSizeClass.Medium -> {
+          SettingsScreen(settings = settings)
+        }
+
+        else -> {
+          Column(
+            modifier = Modifier.width(360.dp),
+          ) {
+            SettingsScreen(settings = settings)
+          }
+        }
+      }
+    }
+
   }
 }
