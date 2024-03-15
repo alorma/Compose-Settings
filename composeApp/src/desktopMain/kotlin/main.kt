7583fd553309694cd720d732c9a57dffad471645
Bernat Borrás <bernat.borras@adevinta.com>
2024-03-15 16:39:44 +0100

    Update desktop

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,11 +1,12 @@
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
   Window(
     onCloseRequest = ::exitApplication,
-    title = "KotlinProject",
+    title = "Compose settings - sample",
   ) {
-    App()
+    App(isSystemInDark = isSystemInDarkTheme())
   }
 }
