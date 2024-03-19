3ba94b8a13f1d2ced91c740630cda2fef5e46f8c
Bernat Borrás <bernat.borras@adevinta.com>
2024-03-19 16:00:10 +0100

    Add preview

---
+++ composeApp/src/androidMain/kotlin/com/alorma/compose/settings/sample/shared/previews/BooleanStateProvider.kt
@@ -0,0 +1,5 @@
+package com.alorma.compose.settings.sample.shared.previews
+
+import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
+
+internal class BooleanStateProvider : CollectionPreviewParameterProvider<Boolean>(listOf(false, true))
--- composeApp/src/androidMain/kotlin/com/alorma/compose/settings/sample/shared/previews/SettingsSwitchTilePreview.kt
+++ composeApp/src/androidMain/kotlin/com/alorma/compose/settings/sample/shared/previews/SettingsSwitchTilePreview.kt
@@ -1,13 +1,24 @@
 package com.alorma.compose.settings.sample.shared.previews
 
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import com.alorma.compose.settings.ui.SettingsSwitch
 import theme.ComposeSettingsTheme
 
-@Preview
+@SuperPreview
 @Composable
-private fun SettingsSwitchTilePreview() {
+private fun SettingsSwitchTilePreview(
+  @PreviewParameter(provider = BooleanStateProvider::class) state: Boolean,
+) {
   ComposeSettingsTheme {
-
+    Surface {
+      SettingsSwitch(
+        state = state,
+        title = { Text(text = "Switch tile") },
+        onCheckedChange = {},
+      )
+    }
   }
 }
