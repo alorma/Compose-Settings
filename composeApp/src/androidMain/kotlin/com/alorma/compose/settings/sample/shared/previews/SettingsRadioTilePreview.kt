d99cb256767a45f87f45ece487d3e92ce9beaa19
Bernat Borrás <bernat.borras@adevinta.com>
2024-03-19 16:05:30 +0100

    Add radio

---
+++ composeApp/src/androidMain/kotlin/com/alorma/compose/settings/sample/shared/previews/SettingsRadioTilePreview.kt
@@ -0,0 +1,24 @@
+package com.alorma.compose.settings.sample.shared.previews
+
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import com.alorma.compose.settings.ui.SettingsRadioButton
+import theme.ComposeSettingsTheme
+
+@SuperPreview
+@Composable
+private fun SettingsCheckboxTilePreview(
+  @PreviewParameter(provider = BooleanStateProvider::class) state: Boolean,
+) {
+  ComposeSettingsTheme {
+    Surface {
+      SettingsRadioButton(
+        state = state,
+        title = { Text(text = "Checkbox tile") },
+        onClick = {},
+      )
+    }
+  }
+}
