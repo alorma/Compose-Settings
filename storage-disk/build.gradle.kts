614d4b120ffcbc544900072e3843035aaef859fc
Bernat Borrás <bernat.borras@adevinta.com>
2024-09-16 08:53:50 +0200

    Remove ExperimentalKotlinGradlePluginApi import

--- storage-disk/build.gradle.kts
+++ storage-disk/build.gradle.kts
@@ -1,5 +1,4 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 plugins {
