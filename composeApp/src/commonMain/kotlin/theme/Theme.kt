e3c715912e220d8fa9fc067ac8011f7cebd15d44
Bernat Borrás <bernat.borras@adevinta.com>
2025-01-05 14:24:38 +0100

    CLeaup

--- composeApp/src/commonMain/kotlin/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/theme/Theme.kt
@@ -1,12 +1,10 @@
 package theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import com.alorma.compose.settings.storage.base.SettingValueState
-import com.alorma.compose.settings.storage.disk.rememberBooleanSettingState
 import com.alorma.compose.settings.storage.memory.rememberMemoryBooleanSettingState
 
 val DarkColorScheme = darkColorScheme(
