774e88c12a5c5d24eba05ad76b5e97e43799038a
Bernat Borrás <bernat.borras@adevinta.com>
2025-01-05 11:59:29 +0100

    Make theme dark

--- composeApp/src/commonMain/kotlin/theme/PlatformColorScheme.kt
+++
@@ -1,7 +0,0 @@
-package theme
-
-import androidx.compose.material3.ColorScheme
-import androidx.compose.runtime.Composable
-
-@Composable
-expect fun createColorScheme(systemInDarkTheme: Boolean): ColorScheme
--- composeApp/src/commonMain/kotlin/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/theme/Theme.kt
@@ -2,10 +2,11 @@ package theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
-val DarkColorScheme = lightColorScheme(
+val DarkColorScheme = darkColorScheme(
   primary = Purple40,
   secondary = PurpleGrey40,
   tertiary = Pink40
@@ -24,7 +25,11 @@ fun ComposeSettingsTheme(
 ) {
 
   MaterialTheme(
-    colorScheme = createColorScheme(isSystemDark),
+    colorScheme = if (isSystemDark) {
+      DarkColorScheme
+    } else {
+      LightColorScheme
+    },
     typography = Typography,
     content = content
   )
