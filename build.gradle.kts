77a27283924728187c030bf89e8b8aa659a9ef00
Bernat Borrás <bernat.borras@adevinta.com>
2025-01-08 13:15:51 +0100

    Update, add dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,56 +1,60 @@
+import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
-import io.gitlab.arturbosch.detekt.Detekt
 
 plugins {
-  alias(libs.plugins.dokka)
-  alias(libs.plugins.jetbrainsCompose) apply false
-  alias(libs.plugins.composeCompiler) apply false
-  alias(libs.plugins.androidApplication) apply false
-  alias(libs.plugins.androidLibrary) apply false
-  alias(libs.plugins.kotlinMultiplatform) apply false
-  alias(libs.plugins.kotlinAndroid) apply false
-  alias(libs.plugins.detekt).apply(false)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.detekt).apply(false)
 }
 
 buildscript {
-  repositories {
-    dependencies {
-      classpath(libs.plugin.gradle.publish)
-      classpath(libs.plugin.gradle.nexus.staging)
-      classpath(libs.plugin.gradle.maven.publish)
+    repositories {
+        dependencies {
+            classpath(libs.plugin.gradle.publish)
+            classpath(libs.plugin.gradle.nexus.staging)
+            classpath(libs.plugin.gradle.maven.publish)
+        }
     }
-  }
 }
 
 apply(plugin = "io.codearte.nexus-staging")
 apply(plugin = "io.github.gradle-nexus.publish-plugin")
 
 allprojects {
-  repositories {
-    google()
-    mavenCentral()
-  }
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 tasks.register("detektAll") {
-  allprojects {
-    this@register.dependsOn(tasks.withType<Detekt>())
-  }
+    allprojects {
+        this@register.dependsOn(tasks.withType<Detekt>())
+    }
+}
+
+tasks.dokkaGeneratePublicationHtml.configure {
+    outputDirectory.set(file("docs/html"))
 }
 
 rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin::class.java) {
-  rootProject.the<YarnRootExtension>().yarnLockMismatchReport = YarnLockMismatchReport.NONE
-  rootProject.the<YarnRootExtension>().reportNewYarnLock = false // true
-  rootProject.the<YarnRootExtension>().yarnLockAutoReplace = false // true
+    rootProject.the<YarnRootExtension>().yarnLockMismatchReport = YarnLockMismatchReport.NONE
+    rootProject.the<YarnRootExtension>().reportNewYarnLock = false // true
+    rootProject.the<YarnRootExtension>().yarnLockAutoReplace = false // true
 }
 
 apply(from = "${rootDir}/scripts/publish-root.gradle")
 
 dependencies {
-  listOf(
-    projects.uiBase,
-    projects.uiTiles,
-    projects.uiTilesExtended,
-  ).forEach { dep -> dokka(dep) }
+    listOf(
+        projects.uiBase,
+        projects.uiTiles,
+        projects.uiTilesExtended,
+    ).forEach { dep -> dokka(dep) }
 }
