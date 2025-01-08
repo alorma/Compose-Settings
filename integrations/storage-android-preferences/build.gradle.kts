d68928b4cd656ab9b42209a6af8d609f4a7ee709
Bernat Borrás <bernat.borras@adevinta.com>
2025-01-08 10:38:15 +0100

    Create integratiosn for android preference

---
+++ integrations/storage-android-preferences/build.gradle.kts
@@ -0,0 +1,104 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+  alias(libs.plugins.kotlinMultiplatform)
+  alias(libs.plugins.androidLibrary)
+  alias(libs.plugins.jetbrainsCompose)
+  alias(libs.plugins.composeCompiler)
+  alias(libs.plugins.detekt)
+}
+
+apply(from = "${rootProject.projectDir}/scripts/publish-module.gradle")
+
+kotlin {
+
+  applyDefaultHierarchyTemplate()
+
+  withSourcesJar()
+
+  androidTarget {
+    publishLibraryVariants("release")
+  }
+
+
+  jvm("desktop")
+
+  iosX64()
+  iosArm64()
+  iosSimulatorArm64()
+
+  js(IR) {
+    browser()
+  }
+
+  @OptIn(ExperimentalWasmDsl::class)
+  wasmJs {
+    browser()
+  }
+
+  sourceSets {
+    androidMain.dependencies {
+      implementation(libs.androidx.preference.preference)
+      implementation(libs.androidx.preference.ktx)
+    }
+
+    commonMain.dependencies {
+      api(projects.storageBase)
+
+      api(libs.multiplatform.settings)
+      api(libs.multiplatform.settings.noArg)
+
+      implementation(compose.runtime)
+      implementation(compose.foundation)
+    }
+  }
+}
+
+android {
+  namespace = libs.versions.namespace.get() + ".disk"
+  compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+  sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+  sourceSets["main"].res.srcDirs("src/androidMain/res")
+  sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+
+  defaultConfig {
+    minSdk = libs.versions.android.minSdk.get().toInt()
+  }
+  packaging {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+  }
+  lint {
+    checkReleaseBuilds = false
+    abortOnError = false
+  }
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  dependencies {
+    debugImplementation(compose.uiTooling)
+  }
+}
+
+compose.desktop {
+  application {
+    nativeDistributions {
+      targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+      packageName = libs.versions.namespace.get() + ".disk"
+      packageVersion = "1.0.0"
+    }
+  }
+}
+
+dependencies {
+  detektPlugins(libs.compose.detekt.rules)
+}
