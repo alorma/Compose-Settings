1c7bf482bf67fdb2b35c0c556153a9c3d558b360
Bernat Borrás <bernat.borras@adevinta.com>
2025-01-09 16:43:04 +0100

    Update sample

--- samples/shared/src/commonMain/kotlin/com/alorma/compose/settings/sample/shared/SampleApp.kt
+++ samples/shared/src/commonMain/kotlin/com/alorma/compose/settings/sample/shared/SampleApp.kt
@@ -1,10 +1,19 @@
 package com.alorma.compose.settings.sample.shared
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SampleApp(modifier: Modifier = Modifier) {
-  Text(text = "Hello world")
+  Scaffold(
+    modifier = modifier,
+    topBar = { TopAppBar(title = { Text(text = "Compose settings") }) },
+  ) {
+    Text(text = "Hello world")
+  }
 }
