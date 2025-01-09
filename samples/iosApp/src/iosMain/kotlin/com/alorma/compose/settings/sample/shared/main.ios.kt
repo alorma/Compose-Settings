08b90d1643d751dd65c5a61c1f3c481e8390a193
Bernat Borrás <bernat.borras@adevinta.com>
2025-01-09 16:41:45 +0100

    Add theme

--- samples/iosApp/src/iosMain/kotlin/com/alorma/compose/settings/sample/shared/main.ios.kt
+++ samples/iosApp/src/iosMain/kotlin/com/alorma/compose/settings/sample/shared/main.ios.kt
@@ -3,9 +3,12 @@ package com.alorma.compose.settings.sample.shared
 
 import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIViewController
+import theme.ComposeSettingsTheme
 
 fun MainViewController(): UIViewController {
   return ComposeUIViewController {
-    SampleApp()
+    ComposeSettingsTheme {
+      SampleApp()
+    }
   }
 }
--- samples/shared/src/iosMain/kotlin/com/alorma/compose/settings/sample/shared/MainViewController.kt
+++
@@ -1,7 +0,0 @@
-package com.alorma.compose.settings.sample.shared
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController {
-  SampleApp()
-}
