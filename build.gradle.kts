30f72a4a2661c3098dac2720b3114df696ad9d37
Bernat Borrás <bernatbor15@gmail.com>
2025-07-25 18:32:14 +0200

    Update android library plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
   alias(libs.plugins.jetbrainsCompose) apply false
   alias(libs.plugins.composeCompiler) apply false
   alias(libs.plugins.androidApplication) apply false
-  alias(libs.plugins.androidLibrary) apply false
+  alias(libs.plugins.androidKmpLibrary) apply false
   alias(libs.plugins.kotlinMultiplatform) apply false
   alias(libs.plugins.kotlinAndroid) apply false
   alias(libs.plugins.detekt).apply(false)
