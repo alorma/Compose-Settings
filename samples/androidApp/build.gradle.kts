66b4721f68db216fccc944405088a472756d5abb
Bernat Borrás <bernatbor15@gmail.com>
2026-01-28 19:17:28 +0100

    AGP 9

--- samples/androidApp/build.gradle.kts
+++ samples/androidApp/build.gradle.kts
@@ -11,36 +11,35 @@ android {
       .get()
       .toInt()
 
-  sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-  sourceSets["main"].res.srcDirs("src/androidMain/res")
-  sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
   defaultConfig {
-    minSdk =
-      libs.versions.android.minSdkSample
-        .get()
-        .toInt()
-    targetSdk =
-      libs.versions.android.targetSdk
-        .get()
-        .toInt()
+    minSdk = libs.versions.android.minSdkSample
+      .get()
+      .toInt()
+
+    targetSdk = libs.versions.android.targetSdk
+      .get()
+      .toInt()
 
     versionCode = 1
     versionName = "1.0"
   }
+
   packaging {
     resources {
       excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
   }
+
   buildFeatures {
     compose = true
   }
+
   buildTypes {
     getByName("release") {
       isMinifyEnabled = false
     }
   }
+
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
