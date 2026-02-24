777a7f48b95c191c308da67f85f6769dc93b5aa1
Bernat Borrás <bernatbor15@gmail.com>
2026-02-24 21:21:24 +0100

    Expressive scaffold

--- ui-tiles-expressive/src/commonMain/kotlin/com/alorma/compose/settings/ui/expressive/SettingsTileDefaults.kt
+++ ui-tiles-expressive/src/commonMain/kotlin/com/alorma/compose/settings/ui/expressive/SettingsTileDefaults.kt
@@ -2,19 +2,26 @@ package com.alorma.compose.settings.ui.expressive
 
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.ListItemElevation
 import androidx.compose.material3.ListItemShapes
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
 import com.alorma.compose.settings.ui.core.SettingsTileCoreDefaults
 
 object SettingsTileDefaults : SettingsTileCoreDefaults() {
   override val Elevation: Dp = ListItemDefaults.Elevation
 
-  @Composable
-  fun shape(): Shape = ListItemDefaults.shape
-
+  /**
+   * Returns the default shapes for expressive Material 3 segmented list items.
+   */
   @OptIn(ExperimentalMaterial3ExpressiveApi::class)
   @Composable
-  fun segmentedShape(): ListItemShapes = ListItemDefaults.shapes()
+  fun shapes(): ListItemShapes = ListItemDefaults.shapes()
+
+  /**
+   * Returns the default elevation for expressive Material 3 segmented list items.
+   */
+  @OptIn(ExperimentalMaterial3ExpressiveApi::class)
+  @Composable
+  fun elevation(): ListItemElevation = ListItemDefaults.elevation()
 }
