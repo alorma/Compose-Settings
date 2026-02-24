24dabe7cb5f6853439bbe6d62cd42d0782912a04
Bernat Borrás <bernatbor15@gmail.com>
2026-02-24 20:59:28 +0100

    Extract core module

--- samples/shared/src/commonMain/kotlin/com/alorma/compose/settings/sample/shared/internal/SampleSection.kt
+++ samples/shared/src/commonMain/kotlin/com/alorma/compose/settings/sample/shared/internal/SampleSection.kt
@@ -9,8 +9,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import com.alorma.compose.settings.ui.SettingsGroup
-import com.alorma.compose.settings.ui.base.internal.LocalSettingsTileColors
 import com.alorma.compose.settings.ui.base.internal.SettingsTileDefaults
+import com.alorma.compose.settings.ui.core.LocalSettingsTileColors
 
 @Composable
 internal fun SampleSection(
